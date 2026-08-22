c9a0115e971c26df8890fd301ca2e39568a78b2c
BrendaBachyPlanchon <bachybrenda@gmail.com>
2026-08-22 18:05:06 +0200

    Préparation du système de commentaires et récupération des éléments de la page

--- js/communaute.js
+++ js/communaute.js
@@ -1,1 +1,17 @@
 console.log("💬 communaute.js chargé");
+
+let zoneEcriture =
+    document.getElementById("zone-ecriture-commentaire");
+
+let champCommentaire =
+    document.getElementById("nouveau-commentaire");
+
+let boutonCommentaire =
+    document.getElementById("envoyer-commentaire");
+
+let messageCommentaire =
+    document.getElementById("message-commentaire");
+
+console.log("📝 Zone commentaire :", zoneEcriture);
+console.log("✏️ Champ commentaire :", champCommentaire);
+console.log("🔘 Bouton commentaire :", boutonCommentaire);
