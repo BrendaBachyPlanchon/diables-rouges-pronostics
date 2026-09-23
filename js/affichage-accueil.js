4dfeefa41351944dba786a2a7f09d35357bdb729
BrendaBachyPlanchon <bachybrenda@gmail.com>
2026-09-23 11:25:57 +0200

    Restauration des drapeaux des équipes nationales sur l'accueil

--- js/affichage-accueil.js
+++ js/affichage-accueil.js
@@ -5,6 +5,69 @@
 
 console.log("✅ affichage-accueil.js serveur actif");
 
+// ==========================================
+// DRAPEAUX DES ÉQUIPES NATIONALES
+// ==========================================
+
+let drapeaux = {
+
+    "Italie": "images/pays/italie.png",
+    "Belgique": "images/pays/belgique.png",
+    "France": "images/pays/france.png",
+    "Turquie": "images/pays/turquie.png",
+    "Espagne": "images/pays/espagne.png",
+
+    "Andorre": "images/pays/andorre.png",
+    "Malte": "images/pays/malte.png",
+    "Serbie": "images/pays/serbie.png",
+    "Grèce": "images/pays/grece.png",
+    "Kosovo": "images/pays/kosovo.png",
+    "Irlande": "images/pays/irlande.png",
+    "Autriche": "images/pays/autriche.png",
+    "Israël": "images/pays/israel.png",
+    "Portugal": "images/pays/portugal.png",
+    "Pays de Galles": "images/pays/pays-galles.png",
+    "Liechtenstein": "images/pays/liechtenstein.png",
+    "Lituanie": "images/pays/lituanie.png",
+    "Pays-Bas": "images/pays/pays-bas.png",
+    "Allemagne": "images/pays/allemagne.png",
+    "Norvège": "images/pays/norvege.png",
+    "Danemark": "images/pays/danemark.png",
+    "Géorgie": "images/pays/georgie.png",
+    "Irlande du Nord": "images/pays/irlande-du-nord.png",
+    "Arménie": "images/pays/armenie.png",
+    "Lettonie": "images/pays/lettonie.png",
+    "Pologne": "images/pays/pologne.png",
+    "Bosnie Herzégovine": "images/pays/bosnie-herzegovine.png",
+    "Suède": "images/pays/suede.png",
+    "Roumanie": "images/pays/roumanie.png",
+    "Monténégro": "images/pays/montenegro.png",
+    "Chypre": "images/pays/chypre.png",
+    "Hongrie": "images/pays/hongrie.png",
+    "Ukraine": "images/pays/ukraine.png",
+    "Slovénie": "images/pays/slovenie.png",
+    "Écosse": "images/pays/ecosse.png",
+    "Féroé": "images/pays/feroe.png",
+    "Kazakhstan": "images/pays/kazakhstan.png",
+    "Bulgarie": "images/pays/bulgarie.png",
+    "Luxembourg": "images/pays/luxembourg.png",
+    "Islande": "images/pays/islande.png",
+    "Estonie": "images/pays/estonie.png",
+    "San Marin": "images/pays/saint-marin.png",
+    "Finlande": "images/pays/finlande.png",
+    "Macédoine du Nord": "images/pays/macedoine-du-nord.png",
+    "Suisse": "images/pays/suisse.png",
+    "Albanie": "images/pays/albanie.png",
+    "Biélorussie": "images/pays/bielorussie.png",
+    "Angleterre": "images/pays/angleterre.png",
+    "Slovaquie": "images/pays/slovaquie.png",
+    "Moldavie": "images/pays/moldavie.png",
+    "République Tchèque": "images/pays/republique-tcheque.png",
+    "Croatie": "images/pays/croatie.png",
+    "Azerbaïdjan": "images/pays/azerbaidjan.png",
+    "Gibraltar": "images/pays/gibraltar.png"
+
+};
 
 // ==========================================
 // CHARGER LES MATCHS DEPUIS SUPABASE
